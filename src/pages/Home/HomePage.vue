c525948525583e795335cff5e857da02587809ad
jair-andrade <jair.andrad.filho@gmail.com>
2024-12-13 11:00:25 -0300

    feat: home page

---
+++ src/pages/Home/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-body">
+    <AppHeader />
+    <LeTipCalculator />
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/Header/AppHeader.vue";
+import LeTipCalculator from "@/components/LeTipCalculator/LeTipCalculator.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    AppHeader,
+    LeTipCalculator,
+  },
+};
+</script>
+
+<style scoped>
+.app-body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
